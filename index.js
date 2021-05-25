5f63ec6e37af98d1e4fbaa9fa6dd92a6908dcf15
Jesse Brown <Brownjesse8895@yahoo.com>
2021-05-25 09:08:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+// Each block in Manhattan is 264 feet long
+// write code that takes in a pick up location for a passenger 
+// and returns the number of blocks from its headquarters 
+
+function distanceFromHqInBlocks(currentLocation){
+    const headQuarters = 42 
+    let numberOfBlocks
+    if(currentLocation > headQuarters){
+       numberOfBlocks = currentLocation - headQuarters
+    } else{ 
+       numberOfBlocks = headQuarters - currentLocation
+    }
+    return numberOfBlocks
+    
+}
+//each block in Manhattan is 264ft long
+function distanceFromHqInFeet(someValue){
+    const distanceInBlcoks = distanceFromHqInBlocks(someValue)
+    const numberInFeet = distanceInBlcoks * 264 
+    return numberInFeet 
+
+}
+
+function distanceTravelledInFeet(currentLocation, locationTravelled){
+    let numberOfBlocks
+    if(currentLocation < locationTravelled){
+        numberOfBlocks = locationTravelled - currentLocation 
+    } else{
+        numberOfBlocks = currentLocation - locationTravelled 
+    }
+    const distanceTravelledInFt = numberOfBlocks * 264 
+    return distanceTravelledInFt
+}
+
+function calculatesFarePrice(start, destination){
+    let youPay
+    if( <= 400){
+        
+    }
+}
+
